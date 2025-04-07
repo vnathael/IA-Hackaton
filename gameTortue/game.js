dbb396e36936b069ef2ad47c01aa288d0e88e604
vnathael <nathael.vigne@ynov.com>
2025-04-07 18:54:01 +0200

    final

--- gameTortue/game.js
+++ gameTortue/game.js
@@ -243,7 +243,7 @@ function handleKeyUp(e) {
 
     // Fonction pour quitter le jeu
     function quitGame() {
-      window.location.href = "/IA-Hackaton/index.html"; // Redirige vers la page d'accueil
+      window.location.href = "index.html"; // Redirige vers la page d'accueil
     }
 
     // Boucle principale du jeu
@@ -284,12 +284,6 @@ function handleKeyUp(e) {
     
         requestAnimationFrame(gameLoop);
     }
-    
-    
-  
-    // Event listeners pour les boutons
-    document.getElementById("quitButton").addEventListener("click", quitGame);
-    document.getElementById("resetButton").addEventListener("click", resetGame);
 
     document.addEventListener('keydown', handleKeyDown);
     document.addEventListener('keyup', handleKeyUp);
